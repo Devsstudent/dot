b35ea12bd4584a75b302d25cb789cba9c7d96d91
Mac 01 <orson.dessein@ecole2600.com>
2024-11-03 00:14:17 +0100

    some work on the universal_installer

--- script_install.sh
+++ script_install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-
 # Next improve is to add a variable that will take MAC or LINUX to know which config to run
 sudo apt update -y && sudo apt-updrade -y
 sudo apt install -y vim bash-completion fzf bat
-
-curl -sS https://starship.rs/install.sh | sh
-
-mkdir -p ~/.config && touch ~/.config/starship.toml
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 ~/.fzf/install
 
+#Pour MAC
+brew update
+brew upgrade
 
+#Common
+curl -sS https://starship.rs/install.sh | sh
+
+mkdir -p ~/.config && touch ~/.config/starship.toml
